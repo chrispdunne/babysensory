205a2dae91cde9af613d1fa9908419991b74a238
stofferd <chrispdunne@gmail.com>
2022-12-22 14:38:09 +0000

    pause before switching to setInterval from requestFrame

--- src/ui.bundle.js
+++ src/ui.bundle.js
@@ -1,5 +1,9 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 function ui() {
+	const _threshold = 0.9;
+	const _filterFreq = 300;
+	const _bufferSize = 2048;
+
 	const container = document.getElementById("url_input");
 	const _input = document.getElementById("yt_url");
 	const audio = document.getElementById("yt_audio");
@@ -25,7 +29,12 @@ function ui() {
 
 	// stop logic
 	let animationFrameId;
-	const stopDisplay = () => cancelAnimationFrame(animationFrameId);
+	let resetMaxMinInterval;
+	const stopDisplay = () => {
+		cancelAnimationFrame(animationFrameId);
+		clearInterval(resetMaxMinInterval);
+	};
+
 	stopBtn.addEventListener("click", stopDisplay);
 
 	const init = () => {
@@ -36,32 +45,38 @@ function ui() {
 		// add low pass filter
 		const loPassFilter = audioContext.createBiquadFilter();
 		loPassFilter.type = "lowpass";
+		loPassFilter.frequency.value = _filterFreq;
 		loPassFilter.connect(audioContext.destination);
 
 		// setup analyser
+		const _sampleRate = audioContext.sampleRate; // 48000
 		const analyser = audioContext.createAnalyser();
-		const _fftSize = 2048;
-		analyser.fftSize = _fftSize;
+		const bufferLengthInSec = _bufferSize / _sampleRate; // 42.666ms or ~1/23 of a second
+		analyser.fftSize = _bufferSize;
 		audioSource.connect(analyser);
 		analyser.connect(loPassFilter);
 		// analyser.connect(audioContext.destination);
 
 		// creates a an array of 2048 elements that can each be a value from 0 to 255
-		var dataArray = new Uint8Array(_fftSize);
+		var dataArray = new Uint8Array(_bufferSize);
 
 		analyser.getByteTimeDomainData(dataArray);
 
-		var max = 0; //89
-		var min = 255; //174
+		var max = 0; //174  ... 196
+		var min = 255; //89 ... 39
+		var getLimit = () => max * _threshold;
+		// after 5 seconds reset max and min
+		// resetMaxMinInterval = setInterval(() => {
+		// 	max = 0;
+		// 	min = 255;
+		// }, 5000);
 
-		// draw a box
-		ctx.fillStyle = "blue";
-		ctx.fillRect(10, 10, 100, 100);
+		var peaksArray = [];
+		var frameCount = 0;
 
 		// display data
+		// each call of display we update the data array with 2048 (buffer size) values of 0 - 255
 		function display() {
-			// console.log("display");
-
 			animationFrameId = requestAnimationFrame(display);
 
 			// update data array
@@ -69,18 +84,27 @@ function ui() {
 
 			// loop through data array
 			dataArray.forEach((eightBitValue, i) => {
+				const limit = getLimit();
 				if (eightBitValue > max) {
 					max = eightBitValue;
 				}
 				if (eightBitValue < min) {
 					min = eightBitValue;
 				}
-				ctx.clearRect(0, 0, canvas.width, canvas.height);
-				// draw a new box
-				ctx.fillStyle = "blue";
-				ctx.fillRect(10, 10, eightBitValue, eightBitValue);
-				// console.log({ max, min });
+
+				// do something if audio louder than threshold
+				if (frameCount > 1 && limit > 100 && eightBitValue > limit) {
+					ctx.clearRect(0, 0, canvas.width, canvas.height);
+					// draw a new box
+					ctx.fillStyle = "blue";
+					ctx.fillRect(10, 10, eightBitValue, eightBitValue);
+					// console.log({ max, min });
+
+					peaksArray.push(frameCount * _bufferSize + i);
+				}
 			});
+			frameCount++;
+			console.log({ peaksArray });
 		}
 		display();
 	};
